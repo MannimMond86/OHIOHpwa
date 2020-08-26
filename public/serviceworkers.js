d9b1774db82f50a71109573a1216c9d03695e963
Tjark Ziehm <github@ohioh.de>
2020-08-26 15:10:14 +0200

    install screen in the browser

--- public/serviceworkers.js
+++ public/serviceworkers.js
@@ -1,13 +1,14 @@
-self.addEventListener("install", function(event){
-    console.log("[Service Worker:] Installing Service Workers ", event)
-});
-self.addEventListener("activated", function(event){
-    console.log("[Service Worker:] Activating Service Workers ", event);
-    return self.clients.claim();
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-self.addEventListener("fetch", function(event){
-    console.log("[Service Worker:] Fetching...", event)
-    event.respondWith(fetch(event.request));
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ...', event);
+  return self.clients.claim();
 });
 
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(fetch(event.request));
+});
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,16 @@
+var deferredPrompt;
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('/serviceworkers.js')
     .then(function() {
       console.log('Service worker registered!');
     });
 }
+
+window.addEventListener('beforeinstallprompt', function(event){
+    console.log("beforeinstallprompt fired");
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
